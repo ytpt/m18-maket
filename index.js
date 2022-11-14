7ee25e32455c4a80c1342689e78b3d9a015bc8b0
Annarchive <ytpt@mail.ru>
2022-11-15 01:55:27 +0800

    refactor styles at style.css add submit action to form at index.js

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ const swiper = new Swiper('.swiper', {
     },
     initialSlide: 1,
 });
+
+const form = document.getElementById('form');
+const formHeader = document.querySelector('.form-header');
+const formFooter = document.querySelector('.form-footer');
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    formHeader.classList.add('hide');
+    formFooter.classList.add('hide');
+    let success = document.createElement('div');
+    success.classList.add('success');
+    success.innerHTML = `
+        <img src='assets/success.svg' alt='Успешно' width='194' height='128'/>
+        <p>Спасибо! Мы будем держать вас в курсе обновлений</p>
+    `;
+    form.replaceWith(success);
+})
