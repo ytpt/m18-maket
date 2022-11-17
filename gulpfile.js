5d99d352ceb135f153729921bc0533780e1b92f6
Annarchive <ytpt@mail.ru>
2022-11-17 23:47:27 +0800

    integrate postcss to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,34 @@
-var gulp = require( 'gulp' );
-var sourcemaps = require( 'gulp-sourcemaps' );
+function defaultTask(cb) {
+    var gulp = require( 'gulp' );
+    var sourcemaps = require( 'gulp-sourcemaps' );
 
-var cssNdb = require( 'gulp-css-nbd' );
-var concat = require( 'gulp-concat' );
+    var cssNdb = require( 'gulp-css-nbd' );
+    var concat = require( 'gulp-concat' );
 
-var postcss = require( 'gulp-postcss' );
-var precss = require( 'precss' );
-var autoprefixer = require( 'autoprefixer' );
-var closest = require( 'postcss-closest' );
+    var postcss = require( 'gulp-postcss' );
+    var precss = require( 'precss' );
+    var autoprefixer = require( 'autoprefixer' );
+    var closest = require( 'postcss-closest' );
 
-var cssNdb = require( 'gulp-css-nbd' );
-var concat = require( 'gulp-concat' );
+    gulp.task(
+        'styles',
+        function ()
+        {
+            var precssOptions = {
+                extension: 'pcss',
+                // Текущая версия postcss-partial-import в PreCSS не поддерживает `dirs`
+                dirs: ['./styles/src/_globals/']
+            };
 
-postcss( [ require( 'postcss-closest' ) ] )
+            gulp.src( './src/style.css' )
+                // .pipe( sourcemaps.init() )
+                // gulp-css-nbd в настоящий момент не работает с sourcemaps
+                .pipe( cssNdb() )
+                .pipe( postcss( [precss( precssOptions ), closest, autoprefixer] ) )
+                .pipe( concat( 'common.css' ) )
+                // .pipe( sourcemaps.write( '.' ) )
+                .pipe( gulp.dest( './styles/' ) );
+        }
+    );
 
-gulp.task(
-    'styles',
-    function ()
-    {
-        var precssOptions = {
-            extension: 'pcss',
-            // Текущая версия postcss-partial-import в PreCSS не поддерживает `dirs`
-            dirs: ['./styles/src/_globals/']
-        };
-
-        gulp.src( './styles/src/**/*.pcss' )
-            // .pipe( sourcemaps.init() )
-            // gulp-css-nbd в настоящий момент не работает с sourcemaps
-            .pipe( cssNdb() )
-            .pipe( postcss( [precss( precssOptions ), closest, autoprefixer] ) )
-            .pipe( concat( 'common.css' ) )
-            // .pipe( sourcemaps.write( '.' ) )
-            .pipe( gulp.dest( './styles/' ) )
-            .pipe( concat( 'common.css' ) )
-            // Some processing (PostCSS, LESS, SCSS or any other with nesting and &)
-            .pipe( gulp.dest( './styles/' ) );
-    }
-);
+exports.default = defaultTask
