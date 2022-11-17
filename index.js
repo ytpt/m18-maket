672517e8e15e84efc843182963afb503710cf3ad
Annarchive <ytpt@mail.ru>
2022-11-17 17:38:28 +0800

    refactor tags name at index.html refactor styles at style.css rebuild slider swiper to swiper on pure JS at index.js

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    speed: 2000,
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 100,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    initialSlide: 1,
-});
+//slider
+const line = document.querySelector('.line');
+const slides = document.querySelectorAll('.slide');
+const sliderWidth = document.querySelector('.slider').offsetWidth;
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+let widthArray = [0];
+let lineWidth = 0;
+let offset = 0;
+let step = 0;
+let rest = 0;
 
+for (let i = 0; i < slides.length; i++) {
+    widthArray.push(slides[i].offsetWidth);
+    lineWidth += slides[i].offsetWidth;
+}
+line.style.width = lineWidth + 'px';
+
+const moveSlide = function() {
+    rest = lineWidth - sliderWidth - (offset + widthArray[step]);
+
+    if(rest >= 0) {
+        offset = offset + widthArray[step];
+        line.style.left = -offset + 'px';
+    } else {
+        line.style.left = -(lineWidth - sliderWidth) + 'px';
+        offset = 0;
+        step = -1;
+    }
+
+    if(step + 1 == slides.length) {
+        step = 0;
+        offset = 0;
+    } else {
+        step++;
+    }
+}
+
+window.onload = moveSlide();
+btnNext.addEventListener('click', moveSlide)
+btnPrev.addEventListener('click', moveSlide);
+
+//form
 const form = document.getElementById('form');
 const formHeader = document.querySelector('.form-header');
 const formFooter = document.querySelector('.form-footer');
