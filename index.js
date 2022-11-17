04e51bf92fa144a6c3a712e09e266c5a5d85d0b0
Annarchive <ytpt@mail.ru>
2022-11-17 23:48:42 +0800

    refactor bugs at index.js and index.html

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //  slider
-const line = document.querySelector( '.line' ;
+const line = document.querySelector( '.line' ) ;
 const slides = document.querySelectorAll( '.slide' );
 const sliderWidth = document.querySelector( '.slider' ).offsetWidth;
 const btnPrev = document.querySelector( '.btn-prev' );
@@ -85,36 +85,3 @@ form.addEventListener( 'submit', function( e )
         form.replaceWith( success );
     }
 } );
-
-//  postcss-closest
-var gulp = require( 'gulp' );
-var sourcemaps = require( 'gulp-sourcemaps' );
-
-var cssNdb = require( 'gulp-css-nbd' );
-var concat = require( 'gulp-concat' );
-
-var postcss = require( 'gulp-postcss' );
-var precss = require( 'precss' );
-var autoprefixer = require( 'autoprefixer' );
-var closest = require( 'postcss-closest' );
-
-gulp.task(
-    'styles',
-    function ()
-    {
-        var precssOptions = {
-            extension: 'pcss',
-            // Текущая версия postcss-partial-import в PreCSS не поддерживает `dirs`
-            dirs: ['./styles/src/_globals/']
-        };
-
-        gulp.src( './styles/src/**/*.pcss' )
-            // .pipe( sourcemaps.init() )
-            // gulp-css-nbd в настоящий момент не работает с sourcemaps
-            .pipe( cssNdb() )
-            .pipe( postcss( [precss( precssOptions ), closest, autoprefixer] ) )
-            .pipe( concat( 'common.css' ) )
-            // .pipe( sourcemaps.write( '.' ) )
-            .pipe( gulp.dest( './styles/' ) );
-    }
-);
